c96223e21393db1350be16d914b753c2d4b7f123
gurpreet8033 <uic.19mca8033@gmail.com>
2021-02-10 14:50:04 +0530

    wallpaper

---
+++ theme.js
@@ -0,0 +1,11 @@
+$(document).ready(()=>{
+    $("#btn1").click(()=>{
+        $("body").css("background-image","url(pinksky.jpg)");
+    })
+    $("#btn2").click(()=>{
+        $("body").css("background-image","url(bluewater.jpg)");
+    })
+    $("#btn3").click(()=>{
+        $("body").css("background-image","url(mask.jpg)");
+    })
+})
